1999a49eabfe487272b08d6a582750a968fcafc3
卢孙仲 <1368062294@qq.com>
2017-10-09 21:12:15 +0800

    change

--- Ultraman/js/Henshen.js
+++ Ultraman/js/Henshen.js
@@ -4,7 +4,17 @@ var big=40;
 //oUltraman.style.width="10%";
 //oUltraman.style.width="6%";
 function bigger () {
+	
 	oUltraman.style.width=big+"%";
-	oUltraman.style.height=big*0.6+"%";
+	oUltraman.style.height='auto';
 	big=big+30;
+	oUltraman.style.display="block";
+//	alert(big);
+	if(big>100){
+	oUltraman.style.width="1200px";
+	oUltraman.style.height="auto";
 }
+	oTime=window.clearInterval('bigger()',1000);
+}
+
+
