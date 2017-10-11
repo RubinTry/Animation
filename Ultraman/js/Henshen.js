1b80c938c4cad04557fcfc948223edcb7221f71c
卢孙仲 <1368062294@qq.com>
2017-10-11 20:52:31 +0800

    add

---
+++ Ultraman/js/Henshen.js
@@ -0,0 +1,20 @@
+var oUltraman=document.getElementById("Ultraman");
+var oTime=window.setInterval('bigger()',1500);
+var big=40;
+//oUltraman.style.width="10%";
+//oUltraman.style.width="6%";
+function bigger () {
+	
+	oUltraman.style.width=big+"%";
+	oUltraman.style.height='auto';
+	big=big+30;
+	oUltraman.style.display="block";
+//	alert(big);
+	if(big>100){
+	oUltraman.style.width="1200px";
+	oUltraman.style.height="auto";
+}
+	oTime=window.clearInterval('bigger()',1000);
+}
+
+
